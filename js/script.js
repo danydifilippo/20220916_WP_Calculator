b9c308d05ea7615d62ba72392d08ae37139a0e65
danydifilippo <danydifilippo@gmail.com>
2022-09-17 19:01:09 +0200

    final
    operations without eval

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
 function restartAC() {
     document.getElementById('display').value='';
+    document.getElementById('disOper').value='';
+    document.getElementById('prevDisplay').value='';
 }
+
 
 
 function n(dato) {
-    document.getElementById('display').value+=dato
+    if ((dato==="+") || (dato==="*") || (dato==="/") || (dato==="-")) {
+        document.getElementById('disOper').value=dato;
+        let firstOp=document.getElementById('display').value
+        document.getElementById('prevDisplay').value=firstOp
+        document.getElementById('display').value=''
+    }
+    else {document.getElementById('display').value+=dato;}
+}
+
+function operation_() {
+    let a=Number(document.getElementById('prevDisplay').value);
+    let b=Number(document.getElementById('display').value);
+    let c=document.getElementById('disOper').value;
+    if (c==="+") {document.getElementById('display').value=a+b}
+ else if(c==="-") {document.getElementById('display').value=a-b}
+ else if(c==="*") {document.getElementById('display').value=a*b}
+ else if(c==="/") {document.getElementById('display').value=a/b}
+ document.getElementById('disOper').value=''
+}
+
+function perc(dato) {
+    let a=Number(document.getElementById('prevDisplay').value);
+    let b=Number(document.getElementById('display').value);
+    let c=document.getElementById('disOper').value;
+    if (c==="+") {document.getElementById('display').value=a+((a*b)/100)}
+ else if(c==="-") {document.getElementById('display').value=a-((a*b)/100)}
+ else if(c==="*") {document.getElementById('display').value=(a*b)/100}
+ else if(c==="/") {document.getElementById('display').value=a/((a*b)/100)}
+ document.getElementById('disOper').value=''
 }
 
 
-function operation() {
-    document.getElementById('display').value=eval(document.getElementById('display').value);
-
-}
